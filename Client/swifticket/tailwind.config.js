428e169230fb0b655032e4a209eaf7ecde1a70cc
rivasdiego-dev <rivasdiego.dev@gmail.com>
2023-05-04 20:54:21 -0600

    Change Tailwind Config
    Rewrite the content of the config file of Tailwind 'tailwind.config.js' to detect all files inside 'pages' and 'components' dirs.

--- Client/swifticket/tailwind.config.js
+++ Client/swifticket/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{html,js,jsx,ts,tsx}',
+    './components/**/*.{html,js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
