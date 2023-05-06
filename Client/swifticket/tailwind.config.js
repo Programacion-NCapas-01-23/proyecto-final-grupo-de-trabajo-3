e4a2c7afca241df5683b22e09cebcd02e6b727e3
rivasdiego-dev <rivasdiego.dev@gmail.com>
2023-05-06 10:44:16 -0600

    Add default margin and padding
    Added deafult margin and padding to solve issue-2

--- Client/swifticket/tailwind.config.js
+++ Client/swifticket/tailwind.config.js
@@ -1,10 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './src/*.{html,js,jsx,ts,tsx}',
+    './src/*/*.{html,js,jsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        'df': '4rem',
+        'df-sm': '2rem',
+        'df-xs': '1rem',
+        'df-lg': '6rem',
+        'df-xl': '8rem',
+        'df-2xl': '16rem',
+      },
+      padding: {
+        'df': '1rem',
+        'df-sm': '0.5rem',
+        'df-xs': '0.25rem',
+        'df-lg': '2rem',
+        'df-xl': '4rem',
+        'df-2xl': '8rem',
+      }
+    },
   },
   plugins: [],
 }
