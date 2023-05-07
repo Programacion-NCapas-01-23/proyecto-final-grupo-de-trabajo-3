ff2ea1b354176b9e561f7e3aa8f429a9c9a404f6
DanielSolis00209020 <00209020@uca.edu.sv>
2023-05-06 19:35:48 -0600

    Start base project for backend
    The default gradle and folders for Spring were created.
    Also, all the necessary initial dependencies for the backend of the web system were added.

---
+++ Server/SwifTicket/src/main/java/com/swifticket/web/SwifTicketApplication.java
@@ -0,0 +1,13 @@
+package com.swifticket.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwifTicketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwifTicketApplication.class, args);
+    }
+
+}
---
+++ Server/SwifTicket/src/test/java/com/swifticket/web/SwifTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swifticket.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwifTicketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
