486ba9c2393bd06a64b492240c0c1786034c03df
rivasdiego-dev <rivasdiego.dev@gmail.com>
2023-05-16 21:12:22 -0600

    Update 'standard' variation of EventCard
    Added the date information to the card view. Also added 2 emtpy files to work on the next 2 variations.

---
+++ Client/swifticket/src/components/EventCards/Shoping/EventCard.jsx
@@ -0,0 +1,6 @@
+
+export default function EventCard() {
+  return (
+    <div>EventCard</div>
+  )
+}
--- Client/swifticket/src/components/EventCards/Standard/EventCard.jsx
+++ Client/swifticket/src/components/EventCards/Standard/EventCard.jsx
@@ -2,15 +2,19 @@ import EventInfo from "../components/EventInfo";
 
 export default function EventCard(props) {
   return (
-    <div className="flex mx-default-xs max-w-[32rem] ">
+    <div className="flex mx-default-xs max-w-[32rem] m-default-xs">
       <div className="relative md:w-[16rem] w-40">
         <img
           className="object-cover transition-all h-full w-full rounded-l-2xl"
           src={props.event.img}
           alt="event_img"
         />
+        <span className="absolute bottom-0 bg-secondary text-center px-3 py-2 rounded-bl-2xl">
+          <p className="text-4xl">{props.event.date_time.getDate()}</p>
+          <p className="uppercase -mt-2">{props.event.date_time.toLocaleString('en-US', { month: 'short' })}</p>
+        </span>
       </div>
-        <EventInfo event={props.event}></EventInfo>
+      <EventInfo event={props.event}></EventInfo>
     </div>
   );
 }
