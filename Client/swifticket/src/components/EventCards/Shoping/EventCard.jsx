307b984ac374bd33583b5c0eea94d0703a4e1b1e
rivasdiego-dev <rivasdiego.dev@gmail.com>
2023-05-17 09:48:29 -0600

    Add second variant for 'EventCard'

--- Client/swifticket/src/components/EventCards/Shoping/EventCard.jsx
+++ Client/swifticket/src/components/EventCards/Shoping/EventCard.jsx
@@ -1,12 +1,15 @@
 
 export default function EventCard(props) {
+  const event = props.event;
+
   return (
-    <div className="flex flex-row w-full max items-center">
+    <div className="flex flex-row w-full max items-center m-default-xs">
       <p className="p-default">{props.count}x</p>
       <div className="border border-primary h-full mr-default-sm"/>
       <span className="bg-secondary p-1 rounded-sm bg-opacity-30 w-32">
-        <img className="object-cover h-24" src={props.event.img} alt="" />
+        <img className="object-cover h-24" src={event.img} alt="" />
       </span>
+      <p className="ml-default-xs"> {event.title}</p>
     </div>
   )
 }
