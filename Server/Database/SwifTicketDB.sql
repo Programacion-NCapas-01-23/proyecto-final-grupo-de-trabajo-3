960846902832d02e9f117a784c5ede4a2117f2ca
carlosxmerca <eduarmercado4@gmail.com>
2023-05-16 22:55:01 -0600

    Update Database File to Work With UUIDs
    The entities that are not catalogs now work with UUIDs as primary keys

--- Server/Database/SwifTicketDB.sql
+++ Server/Database/SwifTicketDB.sql
@@ -49,7 +49,7 @@ CREATE TABLE places (
 
 -- Tablas principales
 CREATE TABLE events (
-  id SERIAL PRIMARY KEY,
+  id uuid NOT NULL DEFAULT gen_random_uuid(),
   category_id INT NOT NULL,
   organizer_id INT NOT NULL,
   state_id INT NOT NULL,
@@ -58,89 +58,96 @@ CREATE TABLE events (
   duration VARCHAR(50) NOT NULL,
   date_time DATE NOT NULL,
   image VARCHAR(100) NOT NULL,
-  FOREIGN KEY (category_id) REFERENCES categories (id),
-  FOREIGN KEY (organizer_id) REFERENCES organizers (id),
-  FOREIGN KEY (state_id) REFERENCES event_states (id),
-  FOREIGN KEY (place_id) REFERENCES places (id)
+  CONSTRAINT event_pk PRIMARY KEY (id),
+  FOREIGN KEY (category_id) REFERENCES categories (id) ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (organizer_id) REFERENCES organizers (id)  ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (state_id) REFERENCES event_states (id) ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (place_id) REFERENCES places (id) ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 CREATE TABLE tiers (
-  id SERIAL PRIMARY KEY,
-  event_id INT NOT NULL,
+  id uuid NOT NULL DEFAULT gen_random_uuid(),
+  event_id uuid NOT NULL,
   name VARCHAR(50) NOT NULL,
   capacity INT NOT NULL,
   price DECIMAL(6,2) NOT NULL,
-  FOREIGN KEY (event_id) REFERENCES events (id)
+  CONSTRAINT tier_pk PRIMARY KEY (id),
+  FOREIGN KEY (event_id) REFERENCES events (id) ON UPDATE CASCADE
 );
 
 CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
-  role_id INT NOT NULL,
+  id uuid NOT NULL DEFAULT gen_random_uuid(),
   state_id INT NOT NULL,
   avatar_id INT,
   name VARCHAR(50) NOT NULL,
   email VARCHAR(100) NOT NULL UNIQUE,
   password VARCHAR NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (role_id) REFERENCES roles (id),
-  FOREIGN KEY (state_id) REFERENCES user_states (id),
-  FOREIGN KEY (avatar_id) REFERENCES avatars (id)
+  CONSTRAINT user_pk PRIMARY KEY (id),
+  FOREIGN KEY (state_id) REFERENCES user_states (id) ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (avatar_id) REFERENCES avatars (id) ON DELETE SET NULL ON UPDATE CASCADE
 );
 
 CREATE TABLE tickets (
-  id SERIAL PRIMARY KEY,
-  user_id INT NOT NULL,
-  tier_id INT NOT NULL,
+  id uuid NOT NULL DEFAULT gen_random_uuid(),
+  user_id uuid NOT NULL,
+  tier_id uuid NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (user_id) REFERENCES users (id),
-  FOREIGN KEY (tier_id) REFERENCES tiers (id)
+  CONSTRAINT ticket_pk PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES users (id) ON UPDATE CASCADE,
+  FOREIGN KEY (tier_id) REFERENCES tiers (id) ON UPDATE CASCADE
 );
 
 CREATE TABLE transactions (
-  id SERIAL PRIMARY KEY,
-  from_id INT NOT NULL,
-  to_id INT NOT NULL,
-  ticket_id INT NOT NULL,
+  id uuid NOT NULL DEFAULT gen_random_uuid(),
+  from_id uuid,
+  to_id uuid NOT NULL,
+  ticket_id uuid,
   req_at TIMESTAMP NOT NULL,
   req_expires_at TIMESTAMP NOT NULL,
-  accept_at TIMESTAMP NOT NULL,
-  accept_expires_at TIMESTAMP NOT NULL,
-  finished_at TIMESTAMP NOT NULL,
-  FOREIGN KEY (from_id) REFERENCES users (id),
-  FOREIGN KEY (to_id) REFERENCES users (id),
-  FOREIGN KEY (ticket_id) REFERENCES tickets (id)
+  accept_at TIMESTAMP,
+  accept_expires_at TIMESTAMP,
+  finished_at TIMESTAMP,
+  CONSTRAINT transaction_pk PRIMARY KEY (id),
+  FOREIGN KEY (from_id) REFERENCES users (id) ON UPDATE CASCADE,
+  FOREIGN KEY (to_id) REFERENCES users (id) ON UPDATE CASCADE,
+  FOREIGN KEY (ticket_id) REFERENCES tickets (id) ON UPDATE CASCADE
 );
 
 CREATE TABLE tokens (
-  id SERIAL PRIMARY KEY,
-  ticket_id INT NOT NULL,
-  verified_at TIMESTAMP NOT NULL,
+  id uuid NOT NULL DEFAULT gen_random_uuid(),
+  ticket_id uuid NOT NULL,
+  verified_at TIMESTAMP,
   expires_at TIMESTAMP NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (ticket_id) REFERENCES tickets (id)
+  CONSTRAINT token_pk PRIMARY KEY (id),
+  FOREIGN KEY (ticket_id) REFERENCES tickets (id) ON UPDATE CASCADE
 );
 
 -- Tablas relación N:N
 CREATE TABLE event_x_validator (
-  id SERIAL PRIMARY KEY,
-  user_id INT NOT NULL,
-  event_id INT NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES users (id),
-  FOREIGN KEY (event_id) REFERENCES events (id)
+  id uuid NOT NULL DEFAULT gen_random_uuid(),
+  user_id uuid NOT NULL,
+  event_id uuid NOT NULL,
+  CONSTRAINT event_x_validator_pk PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES users (id) ON UPDATE CASCADE,
+  FOREIGN KEY (event_id) REFERENCES events (id) ON UPDATE CASCADE
 );
 
 CREATE TABLE event_x_sponsor (
-  id SERIAL PRIMARY KEY,
-  event_id INT NOT NULL,
+  id uuid NOT NULL DEFAULT gen_random_uuid(),
+  event_id uuid NOT NULL,
   sponsor_id INT NOT NULL,
-  FOREIGN KEY (event_id) REFERENCES events (id),
-  FOREIGN KEY (sponsor_id) REFERENCES sponsors (id)
+  CONSTRAINT event_x_sponsor_pk PRIMARY KEY (id),
+  FOREIGN KEY (event_id) REFERENCES events (id) ON UPDATE CASCADE,
+  FOREIGN KEY (sponsor_id) REFERENCES sponsors (id) ON UPDATE CASCADE
 );
 
 CREATE TABLE role_x_users (
-  id SERIAL PRIMARY KEY,
+  id uuid NOT NULL DEFAULT gen_random_uuid(),
   role_id INT NOT NULL,
-  user_id INT NOT NULL,
-  FOREIGN KEY (role_id) REFERENCES roles (id),
-  FOREIGN KEY (user_id) REFERENCES users (id)
+  user_id uuid NOT NULL,
+  CONSTRAINT role_x_users_pk PRIMARY KEY (id),
+  FOREIGN KEY (role_id) REFERENCES roles (id) ON UPDATE CASCADE,
+  FOREIGN KEY (user_id) REFERENCES users (id) ON UPDATE CASCADE
 );
