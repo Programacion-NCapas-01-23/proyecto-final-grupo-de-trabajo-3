16d64f01fb55396d7520b0bf8b69b6835ab0700a
José Miguel Acosta Vásquez <62571916+Acostam331@users.noreply.github.com>
2023-05-18 15:42:10 -0600

    Add: Individual Ticket component
    Individual ticket component with loading and actual ticket web and mobile view

--- Client/swifticket/src/pages/OwnedTickets/OwnedTickets.jsx
+++ Client/swifticket/src/pages/OwnedTickets/OwnedTickets.jsx
@@ -4,11 +4,11 @@ import Ticket from './components/Ticket';
 const OwnedTickets = () => {
   return (
     <div className="flex flex-col w-screen h-screen">
-      <div className="flex items-center gap-4 h-[20vh] ml-16">
+      <div className="flex items-center gap-4 h-[20vh] ml-8 sm:ml-16">
         <span className="h-16 w-1 bg-primary"></span>
-        <p className="title">Mando Cosplays</p>
+        <p className="heading-xl sm:title">Mando Cosplays</p>
       </div>
-      <div className="flex justify-center items-center h-[80vh]">
+      <div className="flex justify-center items-start sm:items-center h-[80vh]">
         <Ticket />
       </div>
     </div>
--- Client/swifticket/src/pages/OwnedTickets/components/Ticket.jsx
+++ Client/swifticket/src/pages/OwnedTickets/components/Ticket.jsx
@@ -11,31 +11,38 @@ const Ticket = () => {
   }, []);
 
   return (
-    <div className="flex justify-center items-center w-3/5 h-4/6 bg-white rounded-[2rem]">
+    <div className="flex flex-col sm:flex-row justify-center items-center w-11/12 sm:w-3/5 h-5/6 sm:h-4/6 bg-white rounded-[2rem] pb-4 sm:pb-0">
       {isLoading ? (
-        <span className="w-1/3 h-full bg-neutral-400 rounded-l-[2rem]" />
+        <span className="w-full sm:w-1/3 h-[30%] sm:h-full bg-neutral-400 sm:rounded-tl-[2rem] sm:rounded-bl-[2rem] sm:rounded-br-none sm:rounded-tr-none rounded-tl-[2rem] rounded-bl-none rounded-br-none rounded-tr-[2rem]" />
       ) : (
         <img
           src={Placeholder}
           alt="Ticket image"
-          className="w-1/3 h-full object-cover rounded-l-[2rem]"
+          className="w-full sm:w-1/3 h-[30%] sm:h-full object-cover sm:rounded-tl-[2rem] sm:rounded-bl-[2rem] sm:rounded-br-none sm:rounded-tr-none rounded-tl-[2rem] rounded-bl-none rounded-br-none rounded-tr-[2rem]"
         />
       )}
-      <div className="flex flex-col justify-center items-center w-1/3 h-full">
+      <div
+        className="flex flex-col justify-center items-center w-full sm:w-1/3 h-[30%] sm:h-full"
+        style={{ display: 'flex' }}
+      >
         {isLoading ? (
-          <img src={LoadingQRPlaceholder} alt="QR" className="w-3/4" />
+          <img
+            src={LoadingQRPlaceholder}
+            alt="QR"
+            className="w-1/2 sm:w-3/4 mt-[-6rem] sm:mt-0"
+          />
         ) : (
           <img
             src={QRPlaceholder}
             alt="QR"
-            className="border-[0.5rem] border-neutral-400 w-3/4 rounded-[0.5rem]"
+            className="border-[0.5rem] border-neutral-400 w-1/2 sm:w-3/4 rounded-[0.5rem] mt-[-6rem] sm:mt-0"
           />
         )}
         <p className="text-black heading-sm">
           {isLoading ? '...' : '484-awd2u233'}
         </p>
       </div>
-      <div className="flex flex-col justify-evenly items-start w-1/3 h-full">
+      <div className="flex flex-col justify-evenly items-start w-full sm:w-1/3 h-2/5 sm:h-full ml-16 sm:ml-0">
         <div className="flex flex-col">
           <p className="text-secondary">Tier</p>
           <p className="heading-lg text-black">
