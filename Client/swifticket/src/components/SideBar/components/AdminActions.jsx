122443917e36612bcc25c421be5111b56eabed5f
rivasdiego-dev <rivasdiego.dev@gmail.com>
2023-05-18 10:11:38 -0600

    Implement 'AdminActions' element

--- Client/swifticket/src/components/SideBar/components/AdminActions.jsx
+++ Client/swifticket/src/components/SideBar/components/AdminActions.jsx
@@ -1,9 +1,23 @@
+import {MdGroups, MdCalendarMonth, MdImportContacts, MdBarChart} from "react-icons/md"
+
 export default function AdminActions() {
     return (
         <li className="py-default text-xl">
-            <a className="flex items-center" href="">
-                <span className="mr-default-xs"> ICON </span>
-                Log In OR My Profile
+            <a className="flex items-center md:my-default-xs my-2" href="">
+                <span className="mr-default-xs"> <MdGroups size={"2rem"}/> </span>
+                All Users
+            </a>
+            <a className="flex items-center md:my-default-xs my-2" href="">
+                <span className="mr-default-xs"> <MdCalendarMonth size={"2rem"}/> </span>
+                Create Event
+            </a>
+            <a className="flex items-center md:my-default-xs my-2" href="">
+                <span className="mr-default-xs"> <MdBarChart size={"2rem"}/> </span>
+                Stats
+            </a>
+            <a className="flex items-center md:my-default-xs my-2" href="">
+                <span className="mr-default-xs"> <MdImportContacts size={"2rem"}/> </span>
+                Catalogs
             </a>
         </li>
     )
