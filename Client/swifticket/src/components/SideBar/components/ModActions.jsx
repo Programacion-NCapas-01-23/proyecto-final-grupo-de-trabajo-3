aa96cabeb1c74c8e41bcfd922cd762992c3dca62
rivasdiego-dev <rivasdiego.dev@gmail.com>
2023-05-18 10:13:42 -0600

    Implement 'ModActions' element

--- Client/swifticket/src/components/SideBar/components/ModActions.jsx
+++ Client/swifticket/src/components/SideBar/components/ModActions.jsx
@@ -1,9 +1,12 @@
+import {MdGroups3} from "react-icons/md"
+
+
 export default function ModActions() {
     return (
         <li className="py-default text-xl">
-            <a className="flex items-center" href="">
-                <span className="mr-default-xs"> ICON </span>
-                Log In OR My Profile
+            <a className="flex items-center md:my-default-xs my-2" href="">
+                <span className="mr-default-xs"> <MdGroups3 size={"2rem"}/> </span>
+                Manage Users
             </a>
         </li>
     )
