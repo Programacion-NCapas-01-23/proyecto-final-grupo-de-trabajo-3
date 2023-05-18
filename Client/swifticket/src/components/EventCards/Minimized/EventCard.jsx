510021ee7fc29e0d081f5465bc5de0a9604baa2e
rivasdiego-dev <rivasdiego.dev@gmail.com>
2023-05-18 10:56:00 -0600

    Add third EventCard variant
    Created the 'Minimized' variant for the EventCard Component.

--- Client/swifticket/src/components/EventCards/Minimized/EventCard.jsx
+++ Client/swifticket/src/components/EventCards/Minimized/EventCard.jsx
@@ -3,10 +3,10 @@ export default function EventCard(props) {
   const event = props.event
 
   return (
-    <div>
-      <span>
-        <img src={event.img} alt="" />
-      </span>
+    <div className="text-center m-default-xs md:w-48 w-36">
+      <div className="p-2 bg-secondary bg-opacity-30 rounded-md">
+        <img className="object-cover md:w-48 md:h-32 w-36 h-20" src={event.img} alt="" />
+      </div>
       <p>{event.title}</p>
     </div>
   )
--- Client/swifticket/src/pages/Testing.jsx
+++ Client/swifticket/src/pages/Testing.jsx
@@ -2,24 +2,55 @@ import EventCard from "../components/EventCards/Minimized/EventCard";
 
 const devEvents = [
   {
-    img: "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/hc_1440x810/public/media/image/2023/02/mandalorian-2964862.jpg?itok=ZMtIO9yv",
+    img: "https://i.imgur.com/CqnHHbp.png",
     title: "Mando Cosplays",
     place: "Richard Vermont Plaza",
     date_time: new Date("2023-06-16T13:00:00"),
   },
   {
-    img: "https://blogthinkbig.com/wp-content/uploads/sites/4/2020/07/Android-Basics-Kotlin.jpg?fit=1500%2C1000",
-    title: "Kotlin Dev Convention",
+    img: "https://i.imgur.com/iqs36FG.png",
+    title: "I am the DANGER",
+    place: "Hell",
+    date_time: new Date("2023-10-06T22:00:00"),
+  },
+  {
+    img: "https://i.imgur.com/NC4hnOQ.png",
+    title: "PS5 Giveaway",
+    place: "Hidden Leaf Village",
+    date_time: new Date("2023-10-08T09:00:00"),
+  },
+  {
+    img: "https://i.imgur.com/XdB0Bdd.png",
+    title: "Hot Labradoodle Eating Contest",
+    place: "El Muelle",
+    date_time: new Date("2023-04-30T12:00:00"),
+  },
+  {
+    img: "https://i.imgur.com/BT7VIvh.png",
+    title: "Travesti Escote Concert",
+    place: "Tilted Towers",
+    date_time: new Date("2023-09-06T16:00:00"),
+  },
+  {
+    img: "https://i.imgur.com/piLVV02.png",
+    title: "Cheesse and Wine",
+    place: "MAGNA VII",
+    date_time: new Date("2023-07-21T18:00:00"),
+  },
+  {
+    img: "https://i.imgur.com/aKDtkl0.png",
+    title: "React Dev Convention",
     place: "UCA",
     date_time: new Date("2023-08-23T09:00:00"),
-  },
+  }
 ];
 
 export default function Testing() {
   return (
-    <div className="flex flex-col w-full h-screen items-center justify-center">
-      <EventCard count={1} event={devEvents[0]}/>
-      <EventCard count={2} event={devEvents[1]}/>
+    <div className="flex flex-wrap w-full h-screen items-center justify-center">
+      {devEvents.map((event, index) => (
+        <EventCard key={index} event={event} />
+      ))}
     </div>
   );
 }
