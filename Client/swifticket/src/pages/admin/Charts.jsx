1d8c4b84bc27ac40a5d0070dca67b6db89c921b5
José Miguel Acosta Vásquez <62571916+Acostam331@users.noreply.github.com>
2023-06-06 16:25:51 -0600

    Add: Admin main page's responsive
    Creation of mobile version of admin page with its components, pending navbar

--- Client/swifticket/src/pages/admin/Charts.jsx
+++ Client/swifticket/src/pages/admin/Charts.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import BarChart from './components/BarChart';
 import LineChart from './components/LineChart';
+import { MdWarning } from 'react-icons/md';
+import Widget from './components/Widget';
 
 const Charts = () => {
+  const widgets = [
+    { id: 1, label: 'Users quantity', value: 450 },
+    { id: 2, label: 'Events held', value: 28 },
+    { id: 3, label: 'Tickets sold', value: 420 },
+    { id: 4, label: 'Event attendance percentage', value: [40, 60] },
+  ];
+
   return (
     <div>
       <div className=" flex h-auto sm:h-screen w-screen">
@@ -10,43 +19,26 @@ const Charts = () => {
           <p>NAV</p>
         </div>
         <div className="flex flex-col h-screen w-screen sm:w-[80vw]">
-          <div className="flex h-[15vh] sm:h-[20vh] w-full justify-start items-end pl-4 sm:pl-8">
+          <div className="flex flex-col sm:flex-row h-[30vh] sm:h-[20vh] w-full justify-between items-start sm:items-end pl-4 sm:pl-8">
             <div className="flex gap-2 justify-center items-center">
               <span className="w-1 h-16 bg-primary" />
               <p className="title">Dashboard</p>
             </div>
+            <div className="flex justify-center items-center w-full sm:w-auto sm:h-full">
+              <button className="flex justify-between items-center px-4 rounded-3xl sm:rounded-r-none sm:rounded-l-3xl w-44 h-9 bg-primary">
+                <MdWarning />
+                <p>Start Services</p>
+                <div className=""></div>
+              </button>
+            </div>
           </div>
           {/* TODO : Fix height */}
-          <div className="flex flex-wrap sm:flex-nowrap h-[45vh] sm:h-[20vh] py-4 w-full justify-evenly items-center">
-            <div className="flex flex-col justify-center items-center h-2/5 sm:h-5/6 min-w-[40%] sm:min-w-[12%] max-w-[50%] sm:max-w-[25%] bg-[#212549] rounded-md">
-              <p>Users quantity</p>
-              <p className="subtitle">450</p>
-            </div>
-            <div className="flex flex-col justify-center items-center h-2/5 sm:h-5/6 min-w-[40%] sm:min-w-[12%] max-w-[50%] sm:max-w-[25%] bg-[#212549] rounded-md">
-              <p>Events held</p>
-              <p className="subtitle">28</p>
-            </div>
-            <div className="flex flex-col justify-center items-center h-2/5 sm:h-5/6 min-w-[40%] sm:min-w-[12%] max-w-[50%] sm:max-w-[25%] bg-[#212549] rounded-md">
-              <p>Tickets sold</p>
-              <p className="subtitle">420</p>
-            </div>
-            <div className="flex flex-col justify-center items-center h-2/5 sm:h-5/6 min-w-[40%] sm:min-w-[18%] max-w-[50%] sm:max-w-[25%] bg-[#212549] rounded-md">
-              <p>Event attendance percentage</p>
-              <div className="flex justify-evenly w-full">
-                <div className="flex flex-col">
-                  <p className="subtitle">40%</p>
-                  <p>Groups</p>
-                </div>
-                <div className="flex flex-col">
-                  <p className="subtitle">/</p>
-                  <p></p>
-                </div>
-                <div className="flex flex-col">
-                  <p className="subtitle">60%</p>
-                  <p>Individuals</p>
-                </div>
-              </div>
-            </div>
+          <div className="flex flex-wrap sm:flex-nowrap h-[40vh] sm:h-[20vh] w-full justify-evenly items-center">
+            {widgets.map((widget) => {
+              const { id, label, value } = widget;
+
+              return <Widget key={id} label={label} value={value} />;
+            })}
           </div>
           <div className="flex flex-col sm:flex-row gap-4 py-4 sm:gap-0 h-[65vh] sm:h-[35vh] w-full justify-evenly items-center">
             <div className="flex justify-center items-center h-[30vh] sm:h-[30vh] w-4/5 sm:w-[35vw] bg-white">
@@ -56,7 +48,7 @@ const Charts = () => {
               <LineChart />
             </div>
           </div>
-          <div className="flex h-[25vh] w-full justify-center items-center">
+          <div className="flex pt-4 pb-8 sm:pt-0 sm:pb-0 sm:h-[25vh] w-full justify-center items-center">
             <button className="bg-primary px-4 py-2 rounded-md">
               Generate Report
             </button>
---
+++ Client/swifticket/src/pages/admin/components/Widget.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const Widget = ({ label, value }) => {
+  const isNumber = typeof value == 'number';
+
+  return (
+    <>
+      {isNumber ? (
+        <div className="flex flex-col justify-center items-center my-2 sm:my-0 h-[16vh] sm:h-5/6 min-w-[40%] sm:min-w-[12%] max-w-[50%] sm:max-w-[25%] bg-[#212549] rounded-md">
+          <p>{label}</p>
+          <p className="subtitle">{value}</p>
+        </div>
+      ) : (
+        <div className="flex flex-col justify-center items-center my-2 sm:my-0 h-[16vh] sm:h-5/6 min-w-[40%] sm:min-w-[18%] max-w-[50%] sm:max-w-[25%] bg-[#212549] rounded-md">
+          <p>{label}</p>
+          <div className="flex justify-evenly w-full">
+            <div className="flex flex-col">
+              <p className="subtitle">{value[0]}%</p>
+              <p>Groups</p>
+            </div>
+            <div className="flex flex-col">
+              <p className="subtitle">/</p>
+              <p></p>
+            </div>
+            <div className="flex flex-col">
+              <p className="subtitle">{value[1]}%</p>
+              <p>Individuals</p>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Widget;
