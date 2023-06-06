c778ffddbfcfd61559ef39df2973ca5221b5e429
rivasdiego-dev <rivasdiego.dev@gmail.com>
2023-06-06 09:08:39 -0600

    Work on checkout page

--- Client/swifticket/src/pages/Checkout/Checkout.jsx
+++ Client/swifticket/src/pages/Checkout/Checkout.jsx
@@ -3,6 +3,9 @@ import PaymentInfo from './PaymentInfo'
 
 export default function Checkout() {
   return (
-    <PaymentInfo></PaymentInfo>
+    <>
+      <h1></h1>
+      <PaymentInfo></PaymentInfo>
+    </>
   )
 }
--- Client/swifticket/src/pages/Checkout/PaymentInfo.jsx
+++ Client/swifticket/src/pages/Checkout/PaymentInfo.jsx
@@ -24,8 +24,23 @@ export default function PaymentInfo() {
 
   const handleInputChange = (evt) => {
     const { name, value } = evt.target;
-
     setState((prev) => ({ ...prev, [name]: value }));
+    console.log({name, value});
+  }
+
+  const handleInputDateChange = (evt) => {
+    const {name, value} = evt.target;
+    
+    const formatedDate = convertDateFormat(value);
+    
+    setState((prev => ({...prev, [name]: formatedDate})));
+
+  }
+
+  function convertDateFormat(dateString) {
+    const [year, month] = dateString.split('-');
+    const formattedDate = `${month}${year.slice(-2)}`;
+    return formattedDate;
   }
 
   const handleInputFocus = (evt) => {
@@ -34,38 +49,41 @@ export default function PaymentInfo() {
 
   return (
     <div className='h-screen w-full flex flex-col items-center justify-center'>
+      <div className='w-min'>
 
-      {/* TARJETA CHINGONA */}
-      <Cards
-        number={state.number}
-        expiry={state.expiry}
-        cvc={state.cvc}
-        name={state.name}
-        focused={state.focus}
-      />
 
-      {/* AQUI ESTAN LOS INPUTS*/}
-      <div className='p-default'>
-      {/* QUE TIENEN VALIDACIONES CHIDAS */}
-        <PaymentInputsWrapper className="w-full text-black mb-default-xs" {...wrapperProps}>
+        {/* TARJETA CHINGONA */}
+        <Cards
+          number={state.number}
+          expiry={state.expiry}
+          cvc={state.cvc}
+          name={state.name}
+          focused={state.focus}
+        />
+
+        {/* AQUI ESTAN LOS INPUTS*/}
+        <div className='py-default'>
+          {/* QUE TIENEN VALIDACIONES CHIDAS */}
+          {/* <PaymentInputsWrapper className="w-full text-black mb-default-xs" {...wrapperProps}>
           <svg {...getCardImageProps({ images })} />
           <input {...getCardNumberProps()} />
           <input {...getExpiryDateProps()} />
           <input {...getCVCProps()} />
-        </PaymentInputsWrapper>
+        </PaymentInputsWrapper> */}
 
 
-        {/* QUE LE DAN ESTILO A LA TAARJETA */}
-        <input className=" w-full p-2 text-black rounded-sm" name='number' onChange={handleInputChange} onFocus={handleInputFocus} placeholder='Card Holder' />
-        <div className='grid grid-cols-4 my-default-xs gap-3'>
-          <input className=" w-full p-2 col-span-2 text-black rounded-sm" name='name' onChange={handleInputChange} onFocus={handleInputFocus} placeholder='Card Holder' />
-          <input className=" w-full p-2 text-black rounded-sm" name='cvc' onChange={handleInputChange} onFocus={handleInputFocus} placeholder='CVC' />
-          <input className=" w-full p-2 text-black rounded-sm" name='expiry' onChange={handleInputChange} onFocus={handleInputFocus} placeholder='Expires' />
+          {/* QUE LE DAN ESTILO A LA TAARJETA */}
+          <input className="p-2 text-black w-full rounded-sm my-default-xs" name='number' type="number" onChange={handleInputChange} onFocus={handleInputFocus} placeholder='Card Number' />
+          <input className="p-2 text-black w-full rounded-sm" name='name' type="text" onChange={handleInputChange} onFocus={handleInputFocus} placeholder='Card Holder' />
+          <div className='grid grid-cols-2 my-default-xs gap-3'>
+            <input className="p-2 text-black w-full rounded-sm" name='cvc' type="number" onChange={handleInputChange} onFocus={handleInputFocus} placeholder='CVC' />
+            <input className="p-2 text-black w-full rounded-sm" name='expiry' type="month" onChange={handleInputDateChange} onFocus={handleInputFocus} placeholder='Expires' />
+          </div>
+
         </div>
 
+
       </div>
-
-
     </div>
   )
 }
