7a05ae3e3e8346a78f41323c258a17e86e56e5e0
José Miguel Acosta Vásquez <62571916+Acostam331@users.noreply.github.com>
2023-06-17 20:37:45 -0600

    Update: Small changes to user side bar

--- Client/swifticket/src/components/SideBar/components/AdminActions.jsx
+++ Client/swifticket/src/components/SideBar/components/AdminActions.jsx
@@ -1,20 +1,27 @@
-import { MdAdminPanelSettings } from "react-icons/md"
+import { MdAdminPanelSettings } from 'react-icons/md';
 import { useNavigate } from 'react-router-dom';
 
 export default function AdminActions() {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const redirectUser = (path) => {
+    navigate(`/${path}`);
+  };
 
-    const redirectUser = (path) => {
-        navigate(`/${path}`);
-    };
-
-    return (
-        <li className="py-default text-xl">
-            <a className="flex items-center md:my-default-xs my-2" onClick={() => {redirectUser("admin")}}>
-                <span className="mr-default-xs"> <MdAdminPanelSettings size={"2rem"}/> </span>
-                Go Admin
-            </a>
-        </li>
-    )
+  return (
+    <li className="py-default text-xl">
+      <a
+        className="flex items-center md:my-default-xs my-2 cursor-pointer"
+        onClick={() => {
+          redirectUser('admin');
+        }}
+      >
+        <span className="mr-default-xs">
+          {' '}
+          <MdAdminPanelSettings size={'2rem'} />{' '}
+        </span>
+        Go Admin
+      </a>
+    </li>
+  );
 }
