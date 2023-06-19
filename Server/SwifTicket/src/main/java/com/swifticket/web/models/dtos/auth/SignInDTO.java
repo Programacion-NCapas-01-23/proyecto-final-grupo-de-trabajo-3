c255f449fc2ad49274ba385ab4127082b42bebb1
carlosxmerca <eduarmercado4@gmail.com>
2023-06-19 08:44:35 -0600

    Fix SignIn DTO :bug:

--- Server/SwifTicket/src/main/java/com/swifticket/web/models/dtos/auth/SignInDTO.java
+++ Server/SwifTicket/src/main/java/com/swifticket/web/models/dtos/auth/SignInDTO.java
@@ -11,12 +11,8 @@ import lombok.Data;
 @AllArgsConstructor
 public class SignInDTO {
 	@NotBlank(message = "email is required")
-	@Email(message = "Format error: email format is required")
 	private String email;
 
 	@NotBlank(message = "password is required")
-	@Size(min = 8, message = "password size is 8 characters min")
-	@Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[!@#$%^&*()_\\-+=<>?])[A-Za-z\\d!@#$%^&*()_\\-+=<>?]+$",
-			message = "Format error: Password must contain 1 upper case, 1 lower case, 1 number, 1 special character")
 	private String password;
 }
