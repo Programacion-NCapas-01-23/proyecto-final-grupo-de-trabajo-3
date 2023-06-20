4e8b3f266b6f593387a8a54bb6b2fe689df93941
rivasdiego-dev <rivasdiego.dev@gmail.com>
2023-06-20 11:03:56 -0600

    Start alerts implementations

--- Client/swifticket/src/pages/Login/components/MainLogin.jsx
+++ Client/swifticket/src/pages/Login/components/MainLogin.jsx
@@ -3,9 +3,11 @@ import { login } from '../../../services/User.Services';
 import { useSetRecoilState } from 'recoil';
 import { tokenState } from '../../../state/atoms/tokenState';
 import { useNavigate } from 'react-router-dom';
+import LoginAlert from '../../../components/Alerts/LoginAlert';
 
 const MainLogin = () => {
   const [userName, setUserName] = useState('');
+  let [hasErrors, setHasErrors] = useState(false);
   const [pass, setPass] = useState('');
   const navigateTo = useNavigate();
 
@@ -15,6 +17,10 @@ const MainLogin = () => {
 
   const loginHandler = async (userName, pass) => {
     const response = await login(userName, pass);
+
+    if (response === undefined || response.status !== 200){
+      setHasErrors(true)
+    }
 
     if (response.status === 200) {
       localStorage.setItem('auth_token', JSON.stringify(response.data.token));
@@ -27,6 +33,7 @@ const MainLogin = () => {
 
   return (
     <div className="container-login flex flex-col justify-between items-center w-full h-full">
+      <LoginAlert hasErrors={hasErrors} setHasErrors={setHasErrors}/>
       <div></div>
       <div className="flex flex-col justify-center sm:justify-evenly items-center gap-y-6 sm:gap-y-0 h-full sm:h-3/5 w-full">
         <div className="flex flex-col justify-evenly items-center w-full h-1/2">
