1904a962050fb018e8a4ee5026f6d4e559a58e18
rivasdiego-dev <rivasdiego.dev@gmail.com>
2023-06-29 15:38:12 -0600

    Work on shopping cart

--- Client/swifticket/src/components/Footer.jsx
+++ Client/swifticket/src/components/Footer.jsx
@@ -1,15 +1,7 @@
-import React, { useRef, useEffect } from "react";
-
 export default function Footer() {
-  const footerRef = useRef(null);
-
-  useEffect(() => {
-    const footerHeight = footerRef.current.getBoundingClientRect().height;
-    console.log("Footer height:", footerHeight);
-  }, []);
 
   return (
-    <footer ref={footerRef} className="bg-gradient-to-t from-default-900 to-default bottom-0 w-full text-center p-default">
+    <footer className="bg-gradient-to-t from-default-900 to-default bottom-0 w-full text-center p-default">
       <p className="text-sm">©2023 Swifticket, Inc. All rights reserved.</p>
     </footer>
   );
--- Client/swifticket/src/pages/OneEvent/OneEvent.jsx
+++ Client/swifticket/src/pages/OneEvent/OneEvent.jsx
@@ -1,34 +1,52 @@
 import { useNavigate, useParams } from "react-router-dom";
 import Landing from "../../Landing";
+import { Toaster, toast } from 'react-hot-toast'
 import { useEffect, useState } from "react";
 import { MdAddBox, MdIndeterminateCheckBox } from "react-icons/md"
 import { getEventById } from "../../services/Events.Services";
+import { shoppingCartState } from "../../state/atoms/shoppingCartState";
+import { useRecoilState } from "recoil";
 
 const OneEvent = () => {
+  const [shoppingCart, setShoooingCart] = useRecoilState(shoppingCartState);
   
   const { eventId } = useParams();
   const navigate = useNavigate();
   const [currentEvent, setCurrentEvent] = useState();
+  const [tierCount, setTierCount] = useState(0);
+
+
+  const handleAddToCart = () => {
+    
+    console.log(currentEvent.tiers);
+
+  }
+  
 
   useEffect(() => {
     const fetchEvent = async () => {
       const response = await getEventById(eventId);
-      console.log(response.data);
-      setCurrentEvent(response.data)
-    }
-    fetchEvent()
-  }, [])
+      const tiers = response.data.tiers;
+      const restructuredTiers = tiers.map(tier => ({ ...tier, count: 0 }));
+
+      const restructuredEvent = { ...response.data, tiers: restructuredTiers };
+      setCurrentEvent(restructuredEvent);
+    };
+
+    fetchEvent();
+  }, []);
+
 
   if (!currentEvent)
     return <div>Loading...</div>;
 
   return (
     <section className={`min-h-[calc(100vh-52px-3.5rem)] max-h-[calc(100vh-52px-4rem)] overflow-x-hidden overflow-y-auto`}>
-
+      <Toaster position="top-right" />
       <div className="">
         <div className="min-h-[calc(40vh-52px-2rem)] relative bg-cover bg-center" style={{ backgroundImage: `url(${currentEvent.image})` }}>
           <div className="min-h-[calc(40vh-52px-2rem)] invisible lg:visible absolute backdrop-blur-lg backdrop-brightness-50 w-full" />
-          <img className="max-h-[calc(40vh-52px-2rem)] invisible lg:visible shadow-2xl absolute top-0 left-1/2 -translate-x-1/2" src={currentEvent.image} alt="" />
+          <img className="max-h-[calc(40vh-52px-2rem)] invisible lg:visible shadow-2xl absolute top-0 left-1/2 -translate-x-1/2 border-4 border-black" src={currentEvent.image} alt="" />
           <div className="min-h-[calc(40vh-52px-2rem)] w-full absolute bg-gradient-to-t from-default  md:via-transparent to-transparent"></div>
         </div>
       </div>
@@ -44,15 +62,37 @@ const OneEvent = () => {
 
         <div className="flex justify-center h-fit">
           <div className="md:w-fit mt-default-sm flex justify-center bg-secondary bg-opacity-30 p-default-sm rounded-lg">
-            <EventTiers event={currentEvent} />
+            <div>
+              {currentEvent.tiers.map((tier, index) => {
+                const handleDecreaseCount = () => {
+                  if (tierCount > 0) {
+                    setTierCount(tierCount - 1);
+                  }
+                };
+                const handleIncreaseCount = () => {
+                  setTierCount(tierCount + 1);
+                };
+                return (
+                  <div key={index} className="grid grid-flow-col grid-cols-3 gap-8 p-default-xs md:text-2xl">
+                    <p className="px-default-sm"> {tier.name} </p>
+                    <p className="px-default-sm"> ${tier.price} </p>
+                    <div className="flex flex-row items-center">
+                      <MdIndeterminateCheckBox onClick={handleDecreaseCount} />
+                      <p className="px-default-sm"> {tierCount} </p>
+                      <MdAddBox onClick={handleIncreaseCount} />
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
 
 
       <div className='flex w-full items-center md:justify-center md:gap-28 justify-evenly mt-default'>
-        <button onClick={() => {navigate(-1)}} className='subaction-button'> Cancel </button>
-        <button className='action-button'> Add To Cart </button>
+        <button onClick={() => { navigate(-1) }} className='subaction-button'> Cancel </button>
+        <button onClick={handleAddToCart} className='action-button'> Add To Cart </button>
       </div>
 
     </section>
@@ -98,40 +138,5 @@ function LinnedText({ text }) {
   )
 }
 
-function EventTiers({ event }) {
-
-  return (
-    <div>
-      {event.tiers.map((tier, index) => {
-        const [tierCount, setTierCount] = useState(0);
-
-        const handleDecreaseCount = () => {
-          if (tierCount > 0) {
-            setTierCount(tierCount - 1);
-          }
-        };
-
-        const handleIncreaseCount = () => {
-          setTierCount(tierCount + 1);
-        };
-
-        return (
-          <div key={index} className="grid grid-flow-col grid-cols-3 gap-8 p-default-xs md:text-2xl">
-            <p className="px-default-sm"> {tier.name} </p>
-            <p className="px-default-sm"> ${tier.price} </p>
-            <div className="flex flex-row items-center">
-              <MdIndeterminateCheckBox onClick={handleDecreaseCount} />
-              <p className="px-default-sm"> {tierCount} </p>
-              <MdAddBox onClick={handleIncreaseCount} />
-            </div>
-          </div>
-        );
-      })}
-    </div>
-  )
-}
-
 
 export default OneEvent;
-
-
