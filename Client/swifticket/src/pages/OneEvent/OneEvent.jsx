ee86381f83a94d412fe5a74eda545dc6c14b8de9
rivasdiego-dev <rivasdiego.dev@gmail.com>
2023-06-29 20:25:35 -0600

    Add shopping cart logic

--- Client/swifticket/src/pages/OneEvent/OneEvent.jsx
+++ Client/swifticket/src/pages/OneEvent/OneEvent.jsx
@@ -8,20 +8,58 @@ import { shoppingCartState } from "../../state/atoms/shoppingCartState";
 import { useRecoilState } from "recoil";
 
 const OneEvent = () => {
-  const [shoppingCart, setShoooingCart] = useRecoilState(shoppingCartState);
-  
+  const [shoppingCart, setShoppingCart] = useRecoilState(shoppingCartState);
+
   const { eventId } = useParams();
   const navigate = useNavigate();
-  const [currentEvent, setCurrentEvent] = useState();
-  const [tierCount, setTierCount] = useState(0);
+  const [currentEvent, setCurrentEvent] = useState(null);
+  const initialTierCounts = {};
 
+  const [tierCounts, setTierCounts] = useState({});
+  const [tierCountsDisplay, setTierCountsDisplay] = useState({});
+
+
+  const shoppingCartValidations = () => {
+    // RESET VISUALS SELECTED
+    currentEvent.tiers.forEach(tier => {
+      initialTierCounts[tier.id] = 0;
+    });
+    setTierCountsDisplay(initialTierCounts)
+
+    // IF NONE SELECTED
+    if (!oneSelected(tierCounts) || !oneSelected(tierCountsDisplay)) {
+      setTierCounts(initialTierCounts)
+      updateEvent()
+      toast.error("You have to select at least one!", { id: 'shcart' })
+      return false
+    }
+
+    if (!checkAvailability(tierCounts, currentEvent.tiers)) {
+      setTierCounts(initialTierCounts)
+      toast.error("Not enough capacity!", { id: 'shcart' })
+      return false
+    }
+    return true
+  }
 
   const handleAddToCart = () => {
-    
-    console.log(currentEvent.tiers);
-
+    if (shoppingCartValidations()) {
+      toast.success("Items added to your cart!", { duration: 2500 })
+      setTierCounts(initialTierCounts)
+      updateEvent()
+    }
   }
-  
+
+  const updateEvent = () => {
+    setCurrentEvent(prev => ({
+      ...prev,
+      tiers: prev.tiers.map(tier => ({
+        ...tier,
+        count: (tier.count || 0) + (tierCounts[tier.id] || 0)
+      }))
+    }));
+  };
+
 
   useEffect(() => {
     const fetchEvent = async () => {
@@ -31,14 +69,21 @@ const OneEvent = () => {
 
       const restructuredEvent = { ...response.data, tiers: restructuredTiers };
       setCurrentEvent(restructuredEvent);
+
+      restructuredTiers.forEach(tier => {
+        initialTierCounts[tier.id] = 0;
+      });
+      setTierCounts(initialTierCounts);
+      setTierCountsDisplay(initialTierCounts)
     };
 
     fetchEvent();
+
   }, []);
 
 
   if (!currentEvent)
-    return <div>Loading...</div>;
+    return <Landing />;
 
   return (
     <section className={`min-h-[calc(100vh-52px-3.5rem)] max-h-[calc(100vh-52px-4rem)] overflow-x-hidden overflow-y-auto`}>
@@ -55,6 +100,7 @@ const OneEvent = () => {
 
 
       <div className="flex md:flex-row flex-col items-center justify-evenly min-h-[calc(30vh-52px-2rem)] md:px-default-2xl px-default-lg pt-default">
+        <button onClick={() => { console.log({ Event: currentEvent }); }}> LOG </button>
         <div className="flex flex-row justify-evenly items-center gap-12">
           <DateInfo event={currentEvent} />
           <EventInfo event={currentEvent} />
@@ -63,23 +109,45 @@ const OneEvent = () => {
         <div className="flex justify-center h-fit">
           <div className="md:w-fit mt-default-sm flex justify-center bg-secondary bg-opacity-30 p-default-sm rounded-lg">
             <div>
-              {currentEvent.tiers.map((tier, index) => {
-                const handleDecreaseCount = () => {
-                  if (tierCount > 0) {
-                    setTierCount(tierCount - 1);
+              {currentEvent.tiers?.map((tier, index) => {
+
+                const handleDecreaseCount = (tierId) => {
+                  if (tierCounts[tierId] > 0) {
+                    setTierCounts(prevCounts => ({
+                      ...prevCounts,
+                      [tierId]: prevCounts[tierId] - 1
+                    }));
+                    setTierCountsDisplay(prevCounts => ({
+                      ...prevCounts,
+                      [tierId]: prevCounts[tierId] - 1
+                    }));
                   }
                 };
-                const handleIncreaseCount = () => {
-                  setTierCount(tierCount + 1);
+
+                const handleIncreaseCount = (tierId) => {
+                  setTierCounts(prevCounts => ({
+                    ...prevCounts,
+                    [tierId]: prevCounts[tierId] + 1
+                  }));
+                  setTierCountsDisplay(prevCounts => ({
+                    ...prevCounts,
+                    [tierId]: prevCounts[tierId] + 1
+                  }));
                 };
+                const tierCount = tierCountsDisplay[tier.id];
                 return (
-                  <div key={index} className="grid grid-flow-col grid-cols-3 gap-8 p-default-xs md:text-2xl">
-                    <p className="px-default-sm"> {tier.name} </p>
-                    <p className="px-default-sm"> ${tier.price} </p>
+
+                  <div key={index} className="grid grid-flow-col grid-cols-4 sm:gap-8 p-default-xs md:text-2xl">
+                    <p className="px-default-sm col-span-2"> {tier.name} </p>
+                    <p className="px-default-sm self-center"> ${tier.price} </p>
                     <div className="flex flex-row items-center">
-                      <MdIndeterminateCheckBox onClick={handleDecreaseCount} />
+                      <button onClick={() => { handleDecreaseCount(tier.id) }}>
+                        <MdIndeterminateCheckBox size="1.5rem" className="text-lg" />
+                      </button>
                       <p className="px-default-sm"> {tierCount} </p>
-                      <MdAddBox onClick={handleIncreaseCount} />
+                      <button onClick={() => { handleIncreaseCount(tier.id) }}>
+                        <MdAddBox size="1.5rem" className="text-lg" />
+                      </button>
                     </div>
                   </div>
                 );
@@ -101,10 +169,10 @@ const OneEvent = () => {
 
 function TitileWithLines({ title }) {
   return (
-    <span className="w-full grid grid-cols-8 px-default-lg pt-default items-center">
-      <div className='border h-0 border-primary'></div>
-      <h1 className='md:title subtitle text-center md:col-span-2 col-span-5'>{title}</h1>
-      <div className='border h-0 border-primary md:col-span-5 col-span-2'></div>
+    <span className="w-full flex px-default-lg pt-default items-center">
+      <div className='border sm:w-default-xl w-default-sm  h-0 border-primary'></div>
+      <h1 className='md:title sm:w-1/2 w-full subtitle text-center'>{title}</h1>
+      <div className='border sm:w-full w-default-sm h-0 border-primary'></div>
     </span>
   )
 }
@@ -127,7 +195,8 @@ function EventInfo({ event }) {
   return (
     <div>
       <LinnedText text={event.place.name} />
-      <LinnedText text={`${date_time.toLocaleString('en-US', { hour: 'numeric', minute: '2-digit', hour12: true })} - 9:00 PM`} />
+      <p className="text-sm sm:text-xl leading-3 -mt-2 font-light tracking-tighter"> {event.place.address} </p>
+      <LinnedText text={`${date_time.toLocaleString('en-US', { hour: 'numeric', minute: '2-digit', hour12: true })}`} />
     </div>
   )
 }
@@ -138,5 +207,31 @@ function LinnedText({ text }) {
   )
 }
 
+function oneSelected(tiersSelected) {
+  if (Object.values(tiersSelected).reduce((acc, curr) => acc + curr, 0) == 0)
+    return false
+  else
+    return true
+}
+
+function checkAvailability(tierCounts, tiers) {
+  for (let i = 0; i < tiers.length; i++) {
+    const tier = tiers[i];
+    const count = tierCounts[tier.id] + tier.count;
+    const remainingCapacity = tier.capacity - tier.ticketsSold;
+    if (count > remainingCapacity) {
+      return false;
+    }
+  }
+  return true;
+};
+
+function addItemsToCart(count, event) {
+  let updatedEventTiers = event.tiers;
+
+  for (let i = 0; i < event.tiers.length; i++) {
+
+  }
+}
 
 export default OneEvent;
