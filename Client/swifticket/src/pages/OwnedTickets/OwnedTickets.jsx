33ad297300cbe71681b709e8926ac19a134b3c3e
carlosxmerca <eduarmercado4@gmail.com>
2023-06-29 21:36:29 -0600

    Update pagination :zap:

--- Client/swifticket/src/pages/OwnedTickets/OwnedTickets.jsx
+++ Client/swifticket/src/pages/OwnedTickets/OwnedTickets.jsx
@@ -67,7 +67,7 @@ function TitileWithLines({ title }) {
 
 function Pagination({page, limit, handlePreviousPage, handleNextPage}) {
   return (
-    <div className="flex flex-wrap w-full justify-evenly mt-[2vh]">
+    <div className="flex flex-wrap w-full justify-center mt-[2vh]">
       {/* next page */}
       { (page > 1) ? 
         <a href="#" className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
