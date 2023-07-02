96e26aec1fa9e0858c933b8bb4113fe352621155
carlosxmerca <eduarmercado4@gmail.com>
2023-07-02 16:32:42 -0600

    Show default img if err :goal_net:

--- Client/swifticket/src/components/EventCards/Minimized/EventCardMi.jsx
+++ Client/swifticket/src/components/EventCards/Minimized/EventCardMi.jsx
@@ -1,8 +1,15 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function EventCardMi(props) {
   
   const navigate = useNavigate();
+
+  const [imageError, setImageError] = useState(false);
+
+  const handleImageError = () => {
+    setImageError(true);
+  };
 
   const redirectUser = () => {
     navigate(`/event/${props.event.id}`);
@@ -14,7 +21,11 @@ export default function EventCardMi(props) {
   return (
     <div className="text-center m-auto md:w-48 w-36 cursor-pointer" onClick={redirectUser}>
       <div className="p-2 bg-secondary bg-opacity-30 rounded-md">
-        <img className="object-cover md:w-48 md:h-32 w-36 h-20" src={event.image} alt="" />
+        {imageError ?
+          <img className="object-cover md:w-48 md:h-32 w-36 h-20" src={`/src/assets/robot.jpg`}  alt="" />
+          :
+          <img className="object-cover md:w-48 md:h-32 w-36 h-20" src={event.image} alt="" onError={handleImageError} />
+        }
       </div>
       <p>{event.title}</p>
     </div>
